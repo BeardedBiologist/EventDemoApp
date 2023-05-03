ffdbb3fac90f71e0f6ad71b8031a24f2f3e44e91
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 14:46:38 +0200

    functionality added

--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -9,17 +9,36 @@ namespace EventDemo
             CollegeClassModel history = new CollegeClassModel("History '101'", 3);
             CollegeClassModel math = new CollegeClassModel("Calculus '201'", 2);
 
+            history.EnrollmentFull += History_EnrollmentFull;
+
             history.SignUpStudents("Tim Corey").PrintToConsole();
             history.SignUpStudents("Sue Storm").PrintToConsole();
             history.SignUpStudents("Joe Smith").PrintToConsole();
             history.SignUpStudents("Mary Jones").PrintToConsole();
             history.SignUpStudents("Sandy Patty").PrintToConsole();
+            Console.WriteLine();
+
+            math.EnrollmentFull += Math_EnrollmentFull;
 
             math.SignUpStudents("Sue Storm").PrintToConsole();
             math.SignUpStudents("Joe Smith").PrintToConsole();
             math.SignUpStudents("Mary Jones").PrintToConsole();
 
             Console.ReadLine();
+        }
+
+        private static void Math_EnrollmentFull(object? sender, string e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("The Enrollment is full for Math");
+            Console.WriteLine();
+        }
+
+        private static void History_EnrollmentFull(object? sender, string e)
+        {
+            Console.WriteLine();
+            Console.WriteLine("The Enrollment is full for History");
+            Console.WriteLine();
         }
     }
 
@@ -33,6 +52,8 @@ namespace EventDemo
 
     public class CollegeClassModel
     {
+        public event EventHandler<string> EnrollmentFull;
+
         private List<string> enrolledStudents = new List<string>();
         private List<string> waitingList = new List<string>();
 
@@ -56,6 +77,10 @@ namespace EventDemo
                 output = $"{ studentName } was enrolled in { CourseTitle }";
 
                 //check to see if we are maxed out
+                if (enrolledStudents.Count == MaximumStudents)
+                {
+                    EnrollmentFull?.Invoke(this, $"{CourseTitle} enrollment is full");
+                }
             }
             else
             {
