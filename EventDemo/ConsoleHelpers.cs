ce33e1f9085080b320d1455057ad154653a6ae72
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 14:53:09 +0200

    final changes and lesson finished.

---
+++ EventDemo/ConsoleHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace EventDemo
+{
+    public static class ConsoleHelpers
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- EventDemo/Program.cs
+++ EventDemo/Program.cs
@@ -9,7 +9,7 @@ namespace EventDemo
             CollegeClassModel history = new CollegeClassModel("History '101'", 3);
             CollegeClassModel math = new CollegeClassModel("Calculus '201'", 2);
 
-            history.EnrollmentFull += History_EnrollmentFull;
+            history.EnrollmentFull += CollegeClass_EnrollmentFull;
 
             history.SignUpStudents("Tim Corey").PrintToConsole();
             history.SignUpStudents("Sue Storm").PrintToConsole();
@@ -18,7 +18,7 @@ namespace EventDemo
             history.SignUpStudents("Sandy Patty").PrintToConsole();
             Console.WriteLine();
 
-            math.EnrollmentFull += Math_EnrollmentFull;
+            math.EnrollmentFull += CollegeClass_EnrollmentFull;
 
             math.SignUpStudents("Sue Storm").PrintToConsole();
             math.SignUpStudents("Joe Smith").PrintToConsole();
@@ -27,68 +27,14 @@ namespace EventDemo
             Console.ReadLine();
         }
 
-        private static void Math_EnrollmentFull(object? sender, string e)
+        private static void CollegeClass_EnrollmentFull(object? sender, string e)
         {
+            CollegeClassModel model = (CollegeClassModel)sender;
             Console.WriteLine();
-            Console.WriteLine("The Enrollment is full for Math");
-            Console.WriteLine();
-        }
-
-        private static void History_EnrollmentFull(object? sender, string e)
-        {
-            Console.WriteLine();
-            Console.WriteLine("The Enrollment is full for History");
+            Console.WriteLine($"{model.CourseTitle}: Full ");
             Console.WriteLine();
         }
-    }
 
-    public static class ConsoleHelpers
-    {
-        public static void PrintToConsole(this string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-
-    public class CollegeClassModel
-    {
-        public event EventHandler<string> EnrollmentFull;
-
-        private List<string> enrolledStudents = new List<string>();
-        private List<string> waitingList = new List<string>();
-
-        public string CourseTitle { get; private set; }
-        public int MaximumStudents { get; private set; }
-
-        public CollegeClassModel(string title, int maximumStudents)
-        {
-            CourseTitle = title;
-            MaximumStudents = maximumStudents;
-
-        }
-
-        public string SignUpStudents(string studentName)
-        {
-            string output = "";
-
-            if (enrolledStudents.Count < MaximumStudents)
-            {
-                enrolledStudents.Add(studentName);
-                output = $"{ studentName } was enrolled in { CourseTitle }";
-
-                //check to see if we are maxed out
-                if (enrolledStudents.Count == MaximumStudents)
-                {
-                    EnrollmentFull?.Invoke(this, $"{CourseTitle} enrollment is full");
-                }
-            }
-            else
-            {
-                waitingList.Add(studentName);
-                output = $"{studentName} was added to the {CourseTitle} waiting list";
-            }
-
-            return output;
-        }
+        
     }
 }
